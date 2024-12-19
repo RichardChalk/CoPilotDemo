0f7eb5df38d57b84b7c6d7856602ec100bfd8cdb
Richard Chalk <barmybrit@gmail.com>
2024-12-19 20:44:00 +0100

    Refactor to generate and display even random numbers
    Removed creation and display of `Person` objects.
    Added `Random` object and `List<int>` named `numbers`.
    Generate 20 random integers between 1 and 100.
    Print only even numbers from the `numbers` list.

--- CoPilotDemo/Program.cs
+++ CoPilotDemo/Program.cs
@@ -4,14 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Person person1 = Person.CreatePersonFromUserInput();
-            Person person2 = Person.CreatePersonFromUserInput();
+            Random random = new Random();
+            List<int> numbers = new List<int>();
 
-            Console.WriteLine("\nPerson 1 Info:");
-            person1.DisplayInfo();
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(random.Next(1, 101)); // Generate random integers between 1 and 100
+            }
 
-            Console.WriteLine("\nPerson 2 Info:");
-            person2.DisplayInfo();
+            Console.WriteLine("Even numbers:");
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
+
+      
     }
 }
